4be1c1e8672252636943792f3959b02699e02557
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-03 19:10:03 +0200

    create nft seed file

--- data/migrations/20210403172415_user_table.js
+++ data/migrations/20210403172415_user_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
     table.increments();
-    table.string("username", 128).notNullable();
+    table.string("username", 128).notNullable().unique();
     table.string("email", 128).notNullable().unique();
     table.string("password", 128).notNullable();
   });
