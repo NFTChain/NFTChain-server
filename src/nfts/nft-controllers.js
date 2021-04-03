b5d55747e76b247aff9c7ca26ac788d52e88f2a2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-03 17:12:54 +0200

    create nft controllers

---
+++ src/nfts/nft-controllers.js
@@ -0,0 +1,21 @@
+require("dotenv");
+
+const Users = require("./nft-model");
+
+exports.getNfts = async (req, res) => {
+  const users = await Users.getAllUsers();
+  if (users) {
+    res.status(200).json(users);
+  } else {
+    res.status(500).json("Users not found");
+  }
+};
+
+exports.getNftByID = async (req, res) => {
+  const user = await Users.findById(req.params.id);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(500).json("User not found");
+  }
+};
