dfe25cc697b9394bb9f01e51135be3b405be5179
Liam Sutton <devliam90@gmail.com>
2021-04-06 22:48:12 +0100

    add dev and start scripts

--- src/nfts/nft-controllers.js
+++ src/nfts/nft-controllers.js
@@ -1,13 +1,13 @@
-require("dotenv");
+require('dotenv');
 
-const Users = require("./nft-model");
+const Users = require('./nft-model');
 
 exports.getNfts = async (req, res) => {
   const users = await Users.getAllNfts();
   if (users) {
     res.status(200).json(users);
   } else {
-    res.status(500).json("Users not found");
+    res.status(500).json('Users not found');
   }
 };
 
@@ -16,6 +16,6 @@ exports.getNftByID = async (req, res) => {
   if (user) {
     res.status(200).json(user);
   } else {
-    res.status(500).json("User not found");
+    res.status(500).json('User not found');
   }
 };
