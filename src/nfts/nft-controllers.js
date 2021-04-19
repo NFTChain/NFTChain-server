2fabb371c3644367fd6cc6c0ccae8390aa366ce7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-19 22:35:14 +0200

    delete unused NFT directory code

--- src/nfts/nft-controllers.js
+++ src/nfts/nft-controllers.js
@@ -1,20 +1,4 @@
 require('dotenv').config();
-const S3 = require('aws-sdk/clients/s3');
-const NFTs = require('./nft-model');
-const fs = require('fs');
-const util = require('util');
-
-const uploadFile = (file) => {
-  const fileStream = fs.createReadStream(file.path);
-
-  const uploadParams = {
-    Bucket: bucketName,
-    Body: fileStream,
-    Key: file.filename,
-  };
-
-  return s3.upload(uploadParams).promise();
-};
 
 exports.uploadNFT = async (req, res) => {
   const file = req.file;
@@ -23,29 +7,4 @@ exports.uploadNFT = async (req, res) => {
   await unlinkFile(file.path);
   console.log(result);
   res.status(200).send({ imagePath: result.key });
-};
-
-function getFileStream(fileKey) {
-  const downloadParams = {
-    Key: fileKey,
-    Bucket: bucketName,
-  };
-
-  return s3.getObject(downloadParams).createReadStream();
-}
-
-exports.getNftByID = async (req, res) => {
-  const key = req.params.id;
-  const readStream = getFileStream(key);
-
-  readStream.pipe(res);
-};
-
-exports.getNfts = async (req, res) => {
-  const users = await NFTs.getAllNfts();
-  if (users) {
-    res.status(200).json(users);
-  } else {
-    res.status(500).json('Users not found');
-  }
 };
--- src/nfts/nft-router.js
+++ src/nfts/nft-router.js
@@ -1,12 +1,8 @@
 require('dotenv');
 const router = require('express').Router();
-const multer = require('multer');
+
 const nftController = require('./nft-controllers');
 
-const upload = multer({ dest: 'uploads/' });
-
-router.get('/', nftController.getNfts);
-router.get('/:id', nftController.getNftByID);
-router.post('/upload', upload.single('image'), nftController.uploadNFT);
+router.post('/upload', nftController.uploadNFT);
 
 module.exports = router;
